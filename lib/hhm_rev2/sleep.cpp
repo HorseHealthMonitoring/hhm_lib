f7741c27eec9bface65f5b476d08f6cab4e4b23c
ajh524 <ajh524@msstate.edu>
2017-04-10 23:27:55 -0500

    Done some work on sleeping. Started revision 2. Ready to compile all pieces in one .ino. Need rest though.

---
+++ lib/hhm_rev2/sleep.cpp
@@ -0,0 +1,26 @@
+#include "sleep.h"
+
+/***************************************************
+ *  Name:        enterSleep
+ *
+ *  Returns:     Nothing.
+ *
+ *  Parameters:  None.
+ *
+ *  Description: Enters the arduino into sleep mode.
+ *
+ ***************************************************/
+void enterSleep(void)
+{
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);   /* EDIT: could also use SLEEP_MODE_PWR_DOWN for lowest power consumption. */
+  sleep_enable();
+  
+  /* Now enter sleep mode. */
+  sleep_mode();
+  
+  /* The program will continue from here after the WDT timeout*/
+  sleep_disable(); /* First thing to do is disable sleep. */
+  
+  /* Re-enable the peripherals. */
+  power_all_enable();
+}
---
+++ lib/hhm_rev2/sleep.h
@@ -0,0 +1,14 @@
+#include <avr/power.h>
+#include <avr/sleep.h>
+
+/***************************************************
+ *  Name:        enterSleep
+ *
+ *  Returns:     Nothing.
+ *
+ *  Parameters:  None.
+ *
+ *  Description: Enters the arduino into sleep mode.
+ *
+ ***************************************************/
+void enterSleep(void);
