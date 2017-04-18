44b70cc50f80aa58e1128ed34a1a116d8938b0fa
ajh524 <ajh524@msstate.edu>
2017-04-18 13:21:49 -0500

    Added images to website.

---
+++ lib/PulseSensorAmped_Arduino_1.5.0/hhm_rev3/hhm_rev3.ino
@@ -0,0 +1,85 @@
+#define LED_PIN (13)
+#define PULSE_PIN (0)
+#define PROCESSING_VISUALIZER 1
+#define SERIAL_PLOTTER 2
+volatile int f_wdt = 1;
+volatile int BPM;
+volatile int rawSignal;
+volatile int IBI = 600;
+volatile boolean pulse = false;
+volatile boolean QS = false;
+
+static int outputType = SERIAL_PLOTTER;
+
+
+/***************************************************
+ *  Name:        setup
+ *
+ *  Returns:     Nothing.
+ *
+ *  Parameters:  None.
+ *
+ *  Description: Setup for the serial comms and the
+ *                Watch dog timeout. 
+ *
+ ***************************************************/
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Initialising...");
+  delay(100); //Allow for serial print to complete.
+
+  pinMode(LED_PIN,OUTPUT);
+
+  /*** Setup the WDT ***/
+  
+  /* Clear the reset flag. */
+  MCUSR &= ~(1<<WDRF);
+  
+  /* In order to change WDE or the prescaler, we need to
+   * set WDCE (This will allow updates for 4 clock cycles).
+   */
+  WDTCSR |= (1<<WDCE) | (1<<WDE);
+
+  /* set new watchdog timeout prescaler value */
+  WDTCSR = 1<<WDP0 | 1<<WDP3; /* 8.0 seconds */
+  
+  /* Enable the WD interrupt (note no reset). */
+  WDTCSR |= _BV(WDIE);
+  
+  Serial.println("Initialisation complete.");
+  delay(100); //Allow for serial print to complete.
+
+  interruptSetup();
+}
+
+
+
+/***************************************************
+ *  Name:        enterSleep
+ *
+ *  Returns:     Nothing.
+ *
+ *  Parameters:  None.
+ *
+ *  Description: Main application loop.
+ *
+ ***************************************************/
+void loop()
+{
+  if(f_wdt == 1)
+  {
+    /* Toggle the LED */
+    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+    
+    /* Don't forget to clear the flag. */
+    f_wdt = 0;
+    
+    /* Re-enter sleep mode. */
+    enterSleep();
+  }
+  else
+  {
+    /* Do nothing. */
+  }
+}
